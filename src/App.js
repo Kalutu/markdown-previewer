6f5e5b7d3121c9a81315721c06638caf1d9c6c73
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-14 15:20:25 +0300

    Create react app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { marked } from 'marked';
+import './style.css';
+
+export default function App() {
+    const [text, setText] = React.useState("# Header\n## Sub header\nThis is a [link](https://example.com)\nHere is some inline `code`.\n```\ncode block\n```\n- List item 1\n- List item 2\n\n> This is a blockquote.\n\n![Image](https://example.com/image.jpg)\n\n**This text is bolded.**");
+
+    const renderer = new marked.Renderer();
+
+    React.useEffect(() => {
+        marked.setOptions({
+            breaks: true
+        });
+    }, [text]);
+
+    function Preview({ markdown }) {
+        const html = marked(markdown, { renderer: renderer });
+        return (
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: html,
+                }}
+            ></div>
+        );
+    }
+
+    return (
+       <div className="text-center px-4">
+            <h1 className="p-2">Markdown Previewer</h1>
+            <textarea name="text" id="editor" rows="10" value={text} className="textarea" onChange={(e) => setText(e.target.value)}></textarea>
+            <h2 className="m-3">Output</h2>
+            <div className="preview-container">
+                <Preview markdown={text} />
+            </div>
+       </div>
+    );
+}
